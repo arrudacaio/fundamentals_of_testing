252a43929b5da2e7b5c3990c84511d861cacb9e0
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2022-05-07 18:08:04 -0300

    Making more helpful error messages

--- assertion-library.js
+++ assertion-library.js
@@ -2,25 +2,44 @@ const { sum, subtract } = require('./math')
 
 let result, expected
 
-result = sum(7, 3)
+function sumTest() {
+  result = sum(7, 3)
+  expected = 9
+  expect(result).toBe(expected)
 
-expected = 10
+}
 
-expect(result).toBe(expected)
+test('Sum adds numbers', sumTest)
 
 
-result = subtract(3, 7)
+function subtractTest() {
+  result = subtract(3, 7)
+  expected = -4
+  expect(result).toBe(expected)
+}
 
-expected = -4
+test('Subtract subtracts numbers', subtractTest)
 
-expect(result).toBe(expected)
+
+// Making more helpful error messages in by running all the tests
+function test(title, callback) {
+  // Como essa função pode lançar um erro, irei colocar dentro de um try
+  try {
+    callback()
+    console.log(`✔️  ${title}`)
+  } catch (error) {
+    console.error(title)
+    console.error(error)
+  }
+
+}
 
 
 function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}.`)
+        throw new Error(`❌ ${actual} is not equal to ${expected}.`)
       }
     },
     toEqual(expected) { },
